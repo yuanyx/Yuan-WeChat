859e1bde46f38a5d7a621ad16c146e382f5ba89a
yuan <yuanyx_155@163.com>
2015-12-02 16:27:23 +0800

    初始化项目

---
+++ WeChat/WeChat/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WeChat
+//
+//  Created by 袁艳祥 on 15/12/2.
+//  Copyright © 2015年 袁艳祥. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
